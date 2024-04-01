1de8e460f8eb9c2bae3647605b03daeb85fe9d06
Vicky S <112182245+vickyviccc@users.noreply.github.com>
2024-04-01 19:55:35 +0200

    Added javascript

---
+++ javascript/app.js
@@ -0,0 +1,27 @@
+
+setInterval(function(){   
+
+let losAngelesElement = document.querySelector("#los-angeles");
+
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+
+
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = `${losAngelesTime.format("h:mm:ss")} <small>${losAngelesTime.format("A")}</small>`;
+
+
+// Paris
+
+let parisElement = document.querySelector("#paris");
+
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/France");
+
+
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = `${parisTime.format("h:mm:ss")} <small>${parisTime.format("A")}</small>`;
+
+}, 1000);
